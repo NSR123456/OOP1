6726cf14d5daf3e8ef15200cdfff3425781a63a6
NSR123456 <83832789+NSR123456@users.noreply.github.com>
2023-08-16 11:11:40 +0600

    Add files via upload

---
+++ Cpp File.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int i= 0, num, age;
+    string name;
+    cout<<"How many friends do you have?\n";
+    cin>>num;
+    cout<<"Enter their names and ages :\n";
+    ofstream MyWriteFile("Friends.txt");
+    while(i < num){
+        cin>>name;
+        MyWriteFile<<" "<< name;
+        cin>>age;
+        MyWriteFile<<" "<<age<<"\n";
+        i++;
+    }
+
+    MyWriteFile.close();
+    string myText;
+    ifstream MyReadFile("Friends.txt");
+    while(getline(MyReadFile, myText)) {
+        cout<< myText;
+    }
+    MyReadFile.close();
+    return 0;
+}
